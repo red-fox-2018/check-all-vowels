73c559e9ed4608b921037ee6df874d37fa147360
Tbfaldhi <tubagusfaldhi@gmail.com>
2018-04-03 19:16:34 +0700

    vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,28 @@
 
+function cekVokal(str){
+  var vokal='aiueoAIUEO'
+  var count=[]
+  for (var i = 0; i < str.length; i++) {
+    for (var j = 0; j < vokal.length; j++) {
+      if(vokal.includes(str)===true){
+        count.push(str[i])
+      }
+
+    }
+    //console.log(count);
+  }
+  //console.log(count);
+  if(count.length==0){
+    return false;
+  }else{
+    return true;
+  }
+
+}
+console.log(cekVokal('auauauauauauauauuauauaup'));//false
+console.log(cekVokal('amkan'));//false
+console.log(cekVokal('aiueo'));//true
+console.log(cekVokal('abbbe'));//false
+console.log(cekVokal('abbae'));//FALSE
+console.log(cekVokal('aabae'));//FALSE
+console.log(cekVokal('AIUEO'));//true

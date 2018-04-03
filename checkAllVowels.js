2b0bffaaaef3c682430b21afcd24f853f15d9360
Renal Apriansyah <renal>
2018-04-03 11:36:23 +0700

    add task check all vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,15 @@
+function checkVowel(str){
+    for(let i=0;i<str.length;i++){
+        if('aiueo'.indexOf(str[i].toLowerCase())==-1){
+            return false
+        }
+    }
+    return true
+}
+
+console.log(checkVowel('tahu bulat'));
+console.log(checkVowel('wyshz'));
+console.log(checkVowel('aaaaiiiii'))
+console.log(checkVowel('aiuOOOeo'));
+
 

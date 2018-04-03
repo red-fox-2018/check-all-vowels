c7f9d343c15ca34c55e4b5c0e700fea1bc0e29eb
ariefardi <forzaariefpyon@gmail.com>
2018-04-03 12:52:19 +0700

    meneyelesaikan fungsi vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,21 @@
-
+function checkVowels(string) {
+	// body...
+	var vocals = ['a','i','u','e','o'];
+	var str = string.split('')
+	for(var i=0;i<str.length;i++){
+		var counter = 0
+		for(var j=0;j<vocals.length;j++){
+			if (vocals[i]===str[j]) {
+				counter++
+			}
+		}
+	}
+	if (counter===0) {
+		return false
+	}
+	return true
+}
+console.log(checkVowels('aieuo'));
+console.log(checkVowels('america'));
+console.log(checkVowels('qrtyp'));
+	

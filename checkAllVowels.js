ff7803d520e4fc844b4c8af75559ac649d35d793
Iswanul Umam <umam@sepulsa.com>
2018-04-03 10:35:11 +0700

    finish checkAllVowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,27 @@
 
+
+/*
+
+@ Iswanul Umam - Red Fox
+*/
+
+let isVowel = function (c) {
+  c = c.toLowerCase();
+  return c == 'a' || c == 'i' || c == 'u' || c == 'e' || c == 'o';
+}
+
+function checkAllVowels(s) {
+  let IsallVowel = true;
+  for (let i of s) {
+    if (!isVowel(i)) {
+      IsallVowel = false;
+    } 
+  }
+  return IsallVowel;
+}
+
+// driver code
+console.log(checkAllVowels('AiUeO')); // true
+console.log(checkAllVowels('aioeo')); // true
+console.log(checkAllVowels('AIUEO')); // true
+console.log(checkAllVowels('NotAllVowels')); // false

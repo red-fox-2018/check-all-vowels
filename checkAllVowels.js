a6f8d17f7468f4a6f28a51064b3fe97529f8d4cf
Natascha Lamsu <tazcha15@gmail.com>
2018-04-03 12:19:36 +0700

    check all vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,16 @@
+function checkVowels(str) {
+  let vocals = ['a', 'i', 'u', 'e', 'o']
+  str = str.toLowerCase();
+  for(let i in vocals) {
+    for(var j = 0; j < str.length; j++) {
+      if(vocals[i] === str[j]) {
+        return true
+      }
+    }
+  }
+  return false
+}
 
+console.log(checkVowels('aaaiiioo'));
+console.log(checkVowels('acha'));
+console.log(checkVowels('AAAOOO'));

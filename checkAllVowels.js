bfa36cfd3c9934ea898b151369ebd6971e0cd008
restuu <restu_u@outlook.com>
2018-04-03 14:08:48 +0700

    first commit

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,19 @@
+function isVowel(str) {
+  var vowel = 'aiueo';
+  var str = str.toLowerCase();
+  for (var i = 0; i < str.length; i++) {
+    var temp = false;
+    for (var j = 0; j < vowel.length; j++) {
+      if (str[i] === vowel[j]) {
+        temp = true;
+      }
+    }
+    if (!temp) {
+      return false;
+    }
+  }
+  return true;
+}
 
+console.log(isVowel('aUUUUiueo'));
+console.log(isVowel('kamu aku dia mereka'));

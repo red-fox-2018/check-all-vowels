3e8fc199255fdaf94db78829092a2f2fd0666c69
alprak <you@example.com>
2018-04-03 12:22:48 +0700

    huruf vokal done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,19 @@
+function checkVowels(kata) {
+  var vokal=['a','i','u','e','o'];
+  
+  for(var i=0;i<vokal.length;i++) {
+  	for(var j=0;j<kata.length;j++) {
+  		if(vokal[i]===kata[j]){
+  			return true
+  		}	
+  	}
+  }
 
+return false;
+
+}
+
+
+console.log(checkVowels('aldo'));
+console.log(checkVowels('fani'));
+console.log(checkVowels('xyz'));

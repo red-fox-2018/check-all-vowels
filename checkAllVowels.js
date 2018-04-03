990c1807d17533520010503663c5246e70440f16
annisaayu <annisaayu569@gmail.com>
2018-04-04 05:49:52 +0700

    check vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
+function allVowels(str){
+    let strSplit = str.split("");
+    let vocal= ['a','i','u','e','o'];
+    let result='';
+    for(let i=0; i<strSplit.length; i++){
+      for(let j=0; j<vocal.length; j++){
+        if(strSplit[i].toLowerCase() === vocal[j]){
+          result += strSplit[i];
+        }
+      }
+    }
 
+    if(str === result){
+      return true
+    }
+    return false
+  }
+
+  console.log(allVowels('aiueoIUoE'));
+  console.log(allVowels('aibueo'));

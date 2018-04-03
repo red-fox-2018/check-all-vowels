240d4288498d71244c2f7b21817b9154d28d5787
Faip Mardoni <faipmardoni@gmail.com>
2018-04-03 12:08:00 +0700

    exercise selesai dengan 2 test case

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,16 @@
+function checkVowel(str) {
+  let arrVocal = ['a', 'e', 'i', 'o', 'u'];
+  let temp = 0;
+  str = str.toLowerCase();
+  for (let i of str) {
+    for (let j of arrVocal) {
+      if (i === j) {
+        temp++
+      }
+    }
+  }
+  return temp === str.length
+}
 
+console.log(checkVowel('aeiou'));
+console.log(checkVowel('faipmardoni'));
